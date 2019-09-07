cce3ac050fd6a590a1c33f2b01e5fab4e189f5c8
hayashiki <peperoncino.pop@gmail.com>
2019-09-08 08:09:01 +0900

    First Commit

---
+++ src/main/kotlin/com/socially/SociallyApplication.kt
@@ -0,0 +1,11 @@
+package com.socially
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SociallyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SociallyApplication>(*args)
+}
---
+++ src/test/kotlin/com/socially/SociallyApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.socially
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SociallyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
